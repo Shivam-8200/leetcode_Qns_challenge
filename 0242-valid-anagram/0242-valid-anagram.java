c5d69e789dd955d2637f128e1df3d5d3546342be
SHIVAM PRASAD <shivamprasad012@gmail.com>
2026-07-02 07:16:32 +0530

    Time: 13 ms (28.65%), Space: 46.8 MB (5.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // Step 1 - length check
-        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        // Step 2 - s ka HashMap
-        HashMap<Character, Integer> smap = new HashMap<>();
-        for(char c : s.toCharArray()){  // String nahi char!
-            if(smap.containsKey(c)){
-                smap.put(c, smap.get(c) + 1);
-            } else {
-                smap.put(c, 1);
-            }
-        }
+// s ke characters add karo
+for(char c : s.toCharArray()){
+    map.put(c, map.getOrDefault(c, 0) + 1);
+}
 
-        // Step 3 - t ka HashMap
-        HashMap<Character, Integer> tmap = new HashMap<>();
-        for(char c : t.toCharArray()){
-            if(tmap.containsKey(c)){
-                tmap.put(c, tmap.get(c) + 1);
-            } else {
-                tmap.put(c, 1);
-            }
-        }
+// t ke characters minus karo
+for(char c : t.toCharArray()){
+    map.put(c, map.getOrDefault(c, 0) - 1);
+}
 
-        // Step 4 - compare karo
-        return smap.equals(tmap);
+// check karo sab 0 hain?
+for(int val : map.values()){
+    if(val != 0) return false;
+}
+return true;
     }
 }
