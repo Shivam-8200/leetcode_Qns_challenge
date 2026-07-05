db4cdfc39a7205dc0bc2948a36b3d33a342468d6
SHIVAM PRASAD <shivamprasad012@gmail.com>
2026-07-05 10:01:30 +0530

    Time: 26 ms (27.61%), Space: 44.9 MB (30.43%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,29 +2,37 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length() > s2.length()) return false;
         
-        int[] s1Count = new int[26];
-        int[] windowCount = new int[26];
+        HashMap<Character, Integer> map = new HashMap<>();
         
-        // s1 ka count banao
+        // s1 ka count
         for(char c : s1.toCharArray()){
-            s1Count[c - 'a']++;
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
-        // pehli window banao
-        for(int i = 0; i < s1.length(); i++){
-            windowCount[s2.charAt(i) - 'a']++;
-        }
+        int count = map.size();  // kitne unique chars match karne hain
+        int left = 0;
         
-        // window slide karo
-        for(int i = s1.length(); i < s2.length(); i++){
-            if(Arrays.equals(s1Count, windowCount)) return true;
+        for(int right = 0; right < s2.length(); right++){
+            char rc = s2.charAt(right);
             
-            // naya character add karo
-            windowCount[s2.charAt(i) - 'a']++;
-            // purana character hatao
-            windowCount[s2.charAt(i - s1.length()) - 'a']--;
+            // naya character
+            if(map.containsKey(rc)){
+                map.put(rc, map.get(rc) - 1);
+                if(map.get(rc) == 0) count--;  // ek character match hua!
+            }
+            
+            // window size s1.length() se badi hui
+            if(right - left + 1 > s1.length()){
+                char lc = s2.charAt(left);
+                if(map.containsKey(lc)){
+                    map.put(lc, map.get(lc) + 1);
+                    if(map.get(lc) == 1) count++;  // match tuta!
+                }
+                left++;
+            }
+            
+            if(count == 0) return true;  // sab match!
         }
-        
-        return Arrays.equals(s1Count, windowCount);
+        return false;
     }
 }
