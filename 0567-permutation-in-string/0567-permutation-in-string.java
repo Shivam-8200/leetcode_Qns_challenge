f827c98b42682ca2affb55be93ed9af29ee8be4d
SHIVAM PRASAD <shivamprasad012@gmail.com>
2026-07-05 10:02:54 +0530

    Time: 6 ms (87.44%), Space: 44.3 MB (38.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,37 +2,29 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length() > s2.length()) return false;
         
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
         
-        // s1 ka count
+        // s1 ka count banao
         for(char c : s1.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            s1Count[c - 'a']++;
         }
         
-        int count = map.size();  // kitne unique chars match karne hain
-        int left = 0;
-        
-        for(int right = 0; right < s2.length(); right++){
-            char rc = s2.charAt(right);
-            
-            // naya character
-            if(map.containsKey(rc)){
-                map.put(rc, map.get(rc) - 1);
-                if(map.get(rc) == 0) count--;  // ek character match hua!
-            }
-            
-            // window size s1.length() se badi hui
-            if(right - left + 1 > s1.length()){
-                char lc = s2.charAt(left);
-                if(map.containsKey(lc)){
-                    map.put(lc, map.get(lc) + 1);
-                    if(map.get(lc) == 1) count++;  // match tuta!
-                }
-                left++;
-            }
-            
-            if(count == 0) return true;  // sab match!
+        // pehli window banao
+        for(int i = 0; i < s1.length(); i++){
+            windowCount[s2.charAt(i) - 'a']++;
         }
-        return false;
+        
+        // window slide karo
+        for(int i = s1.length(); i < s2.length(); i++){
+            if(Arrays.equals(s1Count, windowCount)) return true;
+            
+            // naya character add karo
+            windowCount[s2.charAt(i) - 'a']++;
+            // purana character hatao
+            windowCount[s2.charAt(i - s1.length()) - 'a']--;
+        }
+        
+        return Arrays.equals(s1Count, windowCount);
     }
 }
