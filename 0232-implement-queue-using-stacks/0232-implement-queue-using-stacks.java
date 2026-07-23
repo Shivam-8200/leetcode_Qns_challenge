e1ee9923cd21e32f7405425c8f84a9c740fc797c
SHIVAM PRASAD <shivamprasad012@gmail.com>
2026-07-23 06:06:25 +0530

    Time: 0 ms (100%), Space: 42.6 MB (84.9%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,44 +1,35 @@
-import java.util.Stack;
-
 class MyQueue {
-
-    Stack<Integer> input;
-    Stack<Integer> output;
+    Stack<Integer> stack1;  // inbox
+    Stack<Integer> stack2;  // outbox
 
     public MyQueue() {
-        input = new Stack<>();
-        output = new Stack<>();
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
     }
 
     public void push(int x) {
-        input.push(x);
+        stack1.push(x);  // inbox mein daalo!
     }
 
     public int pop() {
-
-        if (output.isEmpty()) {
-
-            while (!input.isEmpty()) {
-                output.push(input.pop());
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());  // transfer!
             }
         }
-
-        return output.pop();
+        return stack2.pop();  // outbox se nikalo!
     }
 
     public int peek() {
-
-        if (output.isEmpty()) {
-
-            while (!input.isEmpty()) {
-                output.push(input.pop());
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());  // transfer!
             }
         }
-
-        return output.peek();
+        return stack2.peek();  // outbox ka top!
     }
 
     public boolean empty() {
-        return input.isEmpty() && output.isEmpty();
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 }
