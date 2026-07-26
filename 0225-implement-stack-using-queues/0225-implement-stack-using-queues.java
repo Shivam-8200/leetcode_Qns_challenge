e7b6e71ea63dfd7798bc1c603721d78c6f062674
SHIVAM PRASAD <shivamprasad012@gmail.com>
2026-07-26 05:46:19 +0530

    Time: 2 ms (22.92%), Space: 42.8 MB (66.93%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -4,11 +4,11 @@ class MyStack {
     public void push(int x) {
         queue.add(x);
         for(int i = 0; i < queue.size()-1; i++){
-            queue.add(queue.remove());
+            queue.add(queue.poll());
         }
     }
     public int pop() {
-        return queue.remove();
+        return queue.poll();
     }
 
     public int top() {
